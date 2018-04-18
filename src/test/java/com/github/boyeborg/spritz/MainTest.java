43d4c21e90edf7fabd99cac86436b0d116de8c7d
Boye Borg <boye.borg@icloud.com>
2018-04-18 10:20:19 +0200

    convert test file to tabs

--- src/test/java/com/github/boyeborg/spritz/MainTest.java
+++ src/test/java/com/github/boyeborg/spritz/MainTest.java
@@ -5,15 +5,16 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class MainTest extends TestCase {
-    public MainTest(String testName) {
-        super(testName);
-    }
+	
+	public MainTest(String testName) {
+		super(testName);
+	}
 
-    public static Test suite() {
-        return new TestSuite(MainTest.class);
-    }
+	public static Test suite() {
+		return new TestSuite(MainTest.class);
+	}
 
-    public void testMain() {
-        assertTrue(true);
-    }
+	public void testMain() {
+		assertTrue(true);
+	}
 }
