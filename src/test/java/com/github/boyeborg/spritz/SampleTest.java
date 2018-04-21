776346cdbb885ae8f1c58f87ee2b7a3b6a474dfe
Boye Borg <boye.borg@icloud.com>
2018-04-21 18:36:51 +0200

    add sample test

---
+++ src/test/java/com/github/boyeborg/spritz/SampleTest.java
@@ -0,0 +1,20 @@
+package com.github.boyeborg.spritz;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SampleTest extends TestCase {
+	
+	public SampleTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(SampleTest.class);
+	}
+
+	public void testMain() {
+		assertTrue(true);
+	}
+}
